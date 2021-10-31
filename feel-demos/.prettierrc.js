1a70bd29d692940f571bd5057f5d160da1eb81b5
Michael Milewski <saramic@gmail.com>
2021-10-31 13:21:34 +1100

    lint, tslint, prettier and make targets
    make
    make build
    
    with ideas from
      https://codechips.me/eslint-svelte-typescript/

---
+++ feel-demos/.prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSpacing: false,
+  singleQuote: true,
+  printWidth: 90,
+  plugins: ['prettier-plugin-svelte'],
+  semi: false,
+  svelteSortOrder: 'options-styles-scripts-markup',
+  svelteStrictMode: false,
+  svelteBracketNewLine: true,
+  svelteIndentScriptAndStyle: true,
+  trailingComma: 'none'
+}
--- feel-demos/src/App.svelte
+++ feel-demos/src/App.svelte
@@ -1,15 +1,3 @@
-<script lang="ts">
-  import {onMount} from 'svelte'
-
-  let count: number = 0
-  onMount(() => {
-    const interval = setInterval(() => count++, 1000)
-    return () => {
-      clearInterval(interval)
-    }
-  })
-</script>
-
 <style>
   :global(body) {
     margin: 0;
@@ -62,13 +50,30 @@
   }
 </style>
 
+<script lang="ts">
+  import {onMount} from 'svelte'
+
+  let count = 0
+  onMount(() => {
+    const interval = setInterval(() => count++, 1000)
+    return () => {
+      clearInterval(interval)
+    }
+  })
+</script>
+
 <div class="App">
   <header class="App-header">
-    <img src="/logo.svg" class="App-logo" alt="logo"/>
+    <img src="/logo.svg" class="App-logo" alt="logo" />
     <p>Edit <code>src/App.svelte</code> and save to reload.</p>
     <p>Page has been open for <code>{count}</code> seconds.</p>
     <p>
-      <a class="App-link" href="https://svelte.dev" target="_blank" rel="noopener noreferrer">
+      <a
+        class="App-link"
+        href="https://svelte.dev"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         Learn Svelte
       </a>
     </p>
