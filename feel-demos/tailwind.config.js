85d835aa4d80b73d733364a85f6a00ee68cdc2ed
Michael Milewski <saramic@gmail.com>
2021-10-31 13:04:46 +1100

    update the tailwind config as per tutorial

--- feel-demos/tailwind.config.js
+++ feel-demos/tailwind.config.js
@@ -1,11 +1,20 @@
 module.exports = {
-  purge: [],
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  experimental: {
+    uniformColorPalette: true,
+    extendedFontSizeScale: true,
+  },
+  purge: {
+    content: ['./src/**/*.svelte', './public/*.html'],
+    whitelistPatterns: [/svelte-/],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
-  variants: {
-    extend: {},
-  },
+  variants: {},
   plugins: [],
-}
+};
